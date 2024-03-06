4599ffc31a3740d8ec017c96b7bedadc87e0ac87
Ghsjulian <ghsjulian@gmail.com>
2024-03-06 14:47:33 +0600

    Some Changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Our Dependecies
+/*
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -10,8 +11,47 @@ app.get("/",(req,res)=>{
 res.send({"message" : "Welcome From Server"});
 });
 
-
+*/
 /*=================================*/
+/*
+
 app.listen(port, () => {
     console.log(`\n Server is running on port - ${port}\n`);
 });
+*/
+
+
+/*
+
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+
+app.use('/.netlify/functions/api', router);
+
+router.get('/test', (req, res) => {
+  res.send('Hello from Express API!');
+});
+
+module.exports.handler = serverless(app);
+
+*/
+
+
+
+
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const app = express();
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello from Express API!");
+});
+
+app.use("/api/", router);
+
+export const handler = serverless(app);
