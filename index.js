cc5a580a31c8ed05d5fe0d238a3ee9d23921420b
Ghsjulian <ghsjulian@gmail.com>
2024-03-06 16:06:45 +0600

    It's working

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.status(200).send({
+      "status" : "success",
+      "message":"This Is Root Directory"
+  });
 });
 
 app.listen(PORT, () => {
